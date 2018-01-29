61d3baf5707f310217122aad0fc5d04b74850952
Werner Burat <werner.burat@gmail.com>
2018-01-29 09:37:58 -0500

    Ajout du constructeur + ajout de la ligne Attentes dans la méthode affiche().

--- src/CV.java
+++ src/CV.java
@@ -1,28 +1,36 @@
-
 public class CV {
 
 	public String nom, prenom, formation, attentes;
 	public int experienceTravail; // en nombre d'années
 	public String[] competences;
-	
-	public static void main( String[] args ) {
-		System.out.println("Bienvenue chez Barrette!");
 
+	public CV(String nom, String prenom, String formation, String attentes,
+			int experienceTravail, String[] competences) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.attentes = attentes;
+		this.experienceTravail = experienceTravail;
+		this.competences = competences;
 	}
-	
-	public void affiche(){
-		System.out.println( "Nom :\t" + nom + "\r\n" + 
-				"Prenom :\t" + prenom + "\r\n" + 
-				"Formation :\t" + formation + "\r\n" + 
-				"Années d'experience :\t" + experienceTravail+ "\r\n" + 
-				"Competences :\t");
-		for ( int i = 0; i < competences.length; i++ ) {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenue chez Barrette!");
+	}
+
+	public void affiche() {
+		System.out.println("Nom :\t" + nom + "\r\n" + "Prenom :\t" + prenom
+				+ "\r\n" + "Formation :\t" + formation + "\r\n"
+				+ "Années d'experience :\t" + experienceTravail + "\r\n"
+				+ "Competences :\t");
+		for (int i = 0; i < competences.length; i++) {
 			System.out.print(competences[i]);
-			
-			if ( i != competences.length ) {
-				System.out.println(", ");
+
+			if (i != competences.length) {
+				System.out.print(", ");
 			}
 		}
-	}
 
+		System.out.println("\r\nAttentes :\t" + attentes);
+	}
 }
