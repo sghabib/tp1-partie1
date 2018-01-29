3f2387bbcbf75578e7e753b77a964238924f58ff
Werner Burat <werner.burat@gmail.com>
2018-01-29 09:18:22 -0500

    Création des attributs de la classe

--- src/CV.java
+++ src/CV.java
@@ -1,6 +1,10 @@
 
 public class CV {
 
+	public String nom, prenom, formation, attentes;
+	public int experienceTravail; // en nombre d'années
+	public String[] competences;
+	
 	public static void main( String[] args ) {
 		System.out.println("Bienvenue chez Barrette!");
 
