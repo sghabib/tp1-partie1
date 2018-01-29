f27b9bcddb9a3f486fedae18a6759f680411ad13
Mathieu <1710991@cmontmorency.qc.ca>
2018-01-29 09:32:03 -0500

    Ajout de la méthode affiche()

--- src/CV.java
+++ src/CV.java
@@ -9,5 +9,20 @@ public class CV {
 		System.out.println("Bienvenue chez Barrette!");
 
 	}
+	
+	public void affiche(){
+		System.out.println( "Nom :\t" + nom + "\r\n" + 
+				"Prenom :\t" + prenom + "\r\n" + 
+				"Formation :\t" + formation + "\r\n" + 
+				"Années d'experience :\t" + experienceTravail+ "\r\n" + 
+				"Competences :\t");
+		for ( int i = 0; i < competences.length; i++ ) {
+			System.out.print(competences[i]);
+			
+			if ( i != competences.length ) {
+				System.out.println(", ");
+			}
+		}
+	}
 
 }
